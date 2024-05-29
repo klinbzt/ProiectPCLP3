2cd376cff9869d8ed2e97db4cf1e38a1fa2a4ff7
klinbzt <klinbzt@gmail.com>
2024-05-29 14:56:00 +0300

    Am imbunatatit cerinta 3 (Am facut histogramele separat pentru fiecare, in loc de un for).

--- histograme.py
+++ histograme.py
@@ -1,16 +1,58 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Cerinta 3:
+## Cerinta 3:
 data = pd.read_csv('train.csv')
 data_uniq = data_uniq = data.drop_duplicates()
-data_selected = data_uniq.select_dtypes(include=['int64', 'float64'])
 
-for name in data_selected.columns:
-    plt.figure(figsize=(10, 10))
-    plt.hist(data_uniq[name].dropna(), bins=20, color='skyblue', edgecolor='black')
-    plt.title(f'Histograma pentru "{name}"')
-    # plt.xlabel('Valoare')
-    # plt.ylabel('Număr de exemple')
-    plt.grid(True)
-    plt.show()
+## Histograma 'Survived'
+plt.figure(figsize=(10, 10))
+data_uniq['Survived'].plot(kind='hist', bins=[-0.5, 0.5, 1.5], rwidth=0.8, title='Histogram (Survived)')
+plt.xlabel('Survived')
+plt.ylabel('Frequency')
+plt.xticks([0, 1])
+plt.grid(True)
+plt.show()
+
+
+## Histograma 'Pclass'
+plt.figure(figsize=(10, 10))
+data_uniq['Pclass'].plot(kind='hist', bins=[0.5, 1.5, 2.5, 3.5], rwidth=0.8, title='Histogram (Pclass)')
+plt.xlabel('Pclass')
+plt.ylabel('Frequency')
+plt.xticks([1, 2, 3])
+plt.grid(True)
+plt.show()
+
+## Histograma 'Age'
+plt.figure(figsize=(30, 30))
+data_uniq['Age'].plot(kind='hist', bins = list(range(1, 81)), rwidth = 0.6,  title='Histogram (Age)')
+plt.xlabel('Age')
+plt.ylabel('Frequency')
+plt.xticks(list(range(1, 81)))
+plt.grid(True)
+plt.show()
+
+## Histogramă 'SibSp'
+plt.figure(figsize=(10, 6))
+data_uniq['SibSp'].plot(kind='hist', bins=30, title='Histogram (SibSp)')
+plt.xlabel('SibSp')
+plt.ylabel('Frequency')
+plt.grid(True)
+plt.show()
+
+# ## Histograma 'Parch'
+# plt.figure(figsize=(10, 6))
+# data_uniq['Parch'].plot(kind='hist', bins=30, title='Histogram of Parch')
+# plt.xlabel('Parch')
+# plt.ylabel('Frequency')
+# plt.grid(True)
+# plt.show()
+
+# ## Histogramă 'Fare'
+# plt.figure(figsize=(10, 6))
+# data_uniq['Fare'].plot(kind='hist', bins=30, title='Histogram of Fare')
+# plt.xlabel('Fare')
+# plt.ylabel('Frequency')
+# plt.grid(True)
+# plt.show()
