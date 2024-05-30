0fe6a70dc07cce01fbd5b89eae27997d87a7b3ad
klinbzt <klinbzt@gmail.com>
2024-05-30 09:24:33 +0300

    Terminat cerinta 5,6 modificari minore pentru restul

--- cerinta5.py
+++ cerinta5.py
@@ -4,42 +4,29 @@ import matplotlib.pyplot as plt
 # Cerinta 5
 data = pd.read_csv('train_prelucrat.csv')
 
-# Definirea categoriilor de vârstă și a valorii maxime
+## Intervale varste
 bins = [0, 20, 40, 60, 80]
 labels = ['0-20', '21-40', '41-60', '61-80']
 
-# Adăugarea coloanei 'Age_Category' care indică categoria de vârstă
-data['Age_Category'] = pd.cut(data['Age'], bins=bins, labels=labels, right=False)
+## Adaugarea coloana 'Age_interval'
+data['Age_interval'] = pd.cut(data['Age'], bins=bins, labels=labels, right=False)
 
-# Adaugare coloana in tabel
+## Adaugare coloana in tabel
 data.to_csv('train_prelucrat2.csv', index=False)
 
-
-
-
-
-
-
-
-
-
-
-
 data = pd.read_csv('train_prelucrat2.csv')
+data['Age_interval'] = pd.cut(data['Age'], bins=bins, labels=labels, right=False)
 
-data['Age_Category'] = pd.cut(data['Age'], bins=bins, labels=labels, right=False)
+## Numararea persoanelor în fiecare categorie de varsta
+passengers_per_category = data['Age_interval'].value_counts().sort_index()
 
-# Numărarea persoanelor în fiecare categorie de vârstă
-passengers_per_category = data['Age_Category'].value_counts().sort_index()
+## Afisare terminal:
+print(f"Numărul de pasageri în fiecare categorie de vârstă: {passengers_per_category.to_string()}")
 
-# Afișarea rezultatelor
-print("Numărul de pasageri în fiecare categorie de vârstă:")
-print(passengers_per_category)
-
-# Generarea unui grafic pentru a evidenția aceste rezultate
+## Histograma pasageri in fiecare categorie de varsta
 passengers_per_category.plot(kind='bar', color='skyblue')
-plt.title('Numărul de pasageri în fiecare categorie de vârstă')
+plt.title('Numarul de pasageri in fiecare categorie de varsta')
 plt.xlabel('Categorie de vârstă')
-plt.ylabel('Număr de pasageri')
+plt.ylabel('Numar pasageri')
 plt.xticks(rotation=0)
-plt.show()
+plt.savefig("./cerinta5/IntervaleVarsteBarbatiSupr.png")
